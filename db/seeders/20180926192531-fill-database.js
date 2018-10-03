da165105998ab4afd8f1970bfbdedd4102cb858c
Lucas Botteri <lucasbotteri@hotmail.com>
2018-10-03 17:43:54 -0300

    Added seeder and extended expiration

--- db/seeders/20180926192531-fill-database.js
+++ db/seeders/20180926192531-fill-database.js
@@ -4,7 +4,7 @@ const hash = require('@feathersjs/authentication-local/lib/utils/hash');
 module.exports = {
   up: async (queryInterface) => {
     await queryInterface.bulkInsert('user', [
-      { email: 'admin@admin.com', password: await hash('admin'), createdAt: new Date() ,updatedAt: new Date() }
+      { email: 'admin@admin.com', password: await hash('admin'), createdAt: new Date(), updatedAt: new Date() }
     ], {});
 
 
@@ -12,10 +12,10 @@ module.exports = {
       'SELECT * from \"user\";'
     );
     const usersRows = users[0];
-    
+
 
     await queryInterface.bulkInsert('project', [
-      { name: 'proyecto 1', ownerId: usersRows[0].id, createdAt: new Date() ,updatedAt: new Date()}
+      { name: 'proyecto 1', ownerId: usersRows[0].id, createdAt: new Date(), updatedAt: new Date() }
     ], {});
 
 
@@ -23,9 +23,19 @@ module.exports = {
       'SELECT id from project;'
     );
 
-    const projectsRows = projects[0];
     return await queryInterface.bulkInsert('symbol', [
-      { name: 'Cuenta', type: 'Object', projectId: projects[0].id, createdAt: new Date() ,updatedAt: new Date()}
+      { name: 'Cuenta', type: 'OBJECT', projectId: projects[0].id, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Banco', type: 'SUBJECT', projectId: projects[0].id, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Usuario', type: 'VERB', projectId: projects[0].id, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Deposito', type: 'VERB', projectId: projects[0].id, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Extraccion', type: 'VERB', projectId: projects[0].id, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Transaccion', type: 'VERB', projectId: projects[0].id, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Empleado', type: 'SUBJECT', projectId: projects[0].id, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Cuenta Corriente', type: 'OBJECT', projectId: projects[0].id, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Caja Ahorro', type: 'OBJECT', projectId: projects[0].id, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Credito', type: 'VERB', projectId: projects[0].id, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Interes', type: 'VERB', projectId: projects[0].id, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Congelada', type: 'STATE', projectId: projects[0].id, createdAt: new Date(), updatedAt: new Date() },
     ], {});
   },
 
